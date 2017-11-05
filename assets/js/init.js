bd53eb9907650a8a5d1860f67e9b534c79cce769
John Barson <jkbarson@gmail.com>
2017-11-04 21:36:13 -0400

    replace jquery space with document.ready

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,15 +1,13 @@
-(function($){
-  $(function(){
+$(document).ready(function() {
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
 
     var options = [
-        {selector: '#staggered-test', offset: 200, callback: function(el) {
+        {selector: '#staggered-test', offset: 500, callback: function(el) {
             Materialize.showStaggeredList($(el));
         } },
       ];
       Materialize.scrollFire(options);
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+}); // end of document ready
