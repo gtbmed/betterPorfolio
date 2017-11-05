479daa5cad7e59a9f4d42318223a73eef854475b
John Barson <jkbarson@gmail.com>
2017-11-04 21:21:51 -0400

    add transition script

--- assets/js/init.js
+++ assets/js/init.js
@@ -4,5 +4,12 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
 
+    var options = [
+        {selector: '#staggered-test', offset: 200, callback: function(el) {
+            Materialize.showStaggeredList($(el));
+        } },
+      ];
+      Materialize.scrollFire(options);
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
